ba122539aee63ab3e4243d4702ed4a1aff18555d
aayanshchandra2-boop <aayanshchandra2@gmail.com>
2026-06-04 11:13:36 +0530

    conditional statement

---
+++ __pycache__/number_ of_ positive_ or_ negative.py
@@ -0,0 +1,7 @@
+num=input("enter a number:")
+print(type(num))
+num=int(num)
+if num>0:
+    print("number is positive is",num)
+else:
+    print("number is negative:",num)
---
+++ __pycache__/profit_loss.py
@@ -0,0 +1,6 @@
+cp=float(input("enter actual cost"))
+sp=float(input("enter selling cost"))
+if sp>cp:
+ print("profit amount is",sp-cp)
+else:
+ print("no profit ")
