b99ea2e10ee1217bb895d95c16ad0ea8bff8eec0
aayanshchandra2-boop <aayanshchandra2@gmail.com>
2026-06-04 12:37:40 +0530

    understanding weather and wearing cloth according to it

--- __pycache__/number_ of_ positive_ or_ negative.py
+++
@@ -1,7 +0,0 @@
-num=input("enter a number:")
-print(type(num))
-num=int(num)
-if num>0:
-    print("number is positive is",num)
-else:
-    print("number is negative:",num)
--- datatype.py
+++ datatype.py
@@ -1,8 +1,8 @@
-a=53
-print("data type of a is:",type(a),"value of a is",a)
-b=22.9
-print("data type of b is:",type(b),"value of b is",b)
-c="hi"
-print("data type of c is:",type(c),"value of c is",c)
-d=True
-print("data type of d is:",type(d),"value of d is",d)
+actual_cost = float(input(" Please Enter the Actual Product Price: "))
+sale_amount = float(input(" Please Enter the Sales Amount: "))
+ 
+if (sale_amount > actual_cost):
+    amount = sale_amount - actual_cost
+    print("Total Profit = {0}".format(amount))
+else:
+    print("No Profi")
