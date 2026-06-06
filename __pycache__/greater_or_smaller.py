a5fb672cb6c5cdcc95bafe079e5a2f82398b2dc5
aayanshchandra2-boop <aayanshchandra2@gmail.com>
2026-06-06 20:01:37 +0530

    checking the number greater or smaller

---
+++ __pycache__/greater_or_smaller.py
@@ -0,0 +1,8 @@
+a = int(input("enter a number : "))
+if (a < 56):
+    print ("a is little small than 56")
+    print ("i'm in if Block")
+else:
+    print ("a is little greater than 56")
+    print ("i'm in else Block")
+print ("i'm not in if and not in else Block")
