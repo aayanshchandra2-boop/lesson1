9fc7e2c0d475f28e6e2dfe135b1eba4226797337
aayanshchandra2-boop <aayanshchandra2@gmail.com>
2026-06-13 12:02:00 +0530

    bit wise opreator

---
+++ __pycache__/checking_alphabets.py
@@ -0,0 +1,6 @@
+user_input = input("Enter a single character:")
+# Check if it is an alphabet
+if 
+        print("The given character is an Alphabet.")
+else:
+        print("The given character is not an Alphabet.")
---
+++ bit_wise_opreator.py
@@ -0,0 +1,9 @@
+a=10
+b=-10
+#print bit wise right shift opreator
+print("a>>1=",a>>1)
+print("b>>1=",b>>1)
+a=5
+#print bit wise left shift opreator
+print("a<<1=",a<<1)
+print("b<<1=",b<<1)
