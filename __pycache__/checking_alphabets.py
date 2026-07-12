f2287249b483c38fa40a25922e1118dfaf9c4750
aayanshchandra2-boop <aayanshchandra2@gmail.com>
2026-07-12 10:29:34 +0530

    nested conditional statement

--- __pycache__/checking_alphabets.py
+++ __pycache__/checking_alphabets.py
@@ -1,6 +1,6 @@
 user_input = input("Enter a single alphabet character (A-Z):")
 # Check if it is an alphabet
-if user_input:
-        print("The given character is an alphabet.")
-else:
-        print("The given character is not an alphabet.")
+if (user_input>='a' and user_input<='z'):
+    print("The given character is an alphabet.")
+else (user_input>='a' and user_input<='z'):
+    print("The given character is not an alphabet.")
